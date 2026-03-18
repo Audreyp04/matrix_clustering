62273a37d73c4d01b4c0d12206a6343659c1f10f
Audrey Prendergast <audrey.prendergast2004@gmail.com>
2026-03-18 16:21:43 -0400

    Update run_matrix_clustering.py

--- run_matrix_clustering.py
+++ run_matrix_clustering.py
@@ -2,6 +2,16 @@ import numpy as np
 import silhouette
 import prep_matrix
 
+prep_matrix.flags2variables()
+prep_matrix.get_index2seq_dic()
+prep_matrix.recolumn()
+prep_matrix.read_pairdata()
+prep_matrix.get_blank_matrix()
+prep_matrix.write_weights_for_clustering()
+prep_matrix.weight_pair_data()
+
+
+
 X = np.load('weights.npy')
 S = silhouette.do_silhouette(X)
 df = silhouette.do_scatterplot(silhouette.best_n, X)
